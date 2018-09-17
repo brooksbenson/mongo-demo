0e8a11c8fbd9e95720b1ddb35e80ada998388092
Brooks Benson <brooksbenson01@gmail.com>
2018-09-17 13:18:19 -0600

    schemas

--- schemas.js
+++ schemas.js
@@ -6,16 +6,77 @@ mongoose
   .catch(() => console.error('connection failed'));
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['backend', 'frontend', 'programming'],
+    lowercase: true
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 25
+  },
+  tags: {
+    type: Array,
+    validate: {
+      validator: function(v) {
+        console.log(v);
+        return v && v.length > 0;
+      },
+      message: 'tags must be an array and have a length greater than 0'
+    }
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 5,
+    max: 20
+  },
+  rating: {
+    type: Number,
+    required: true,
+    validate: {
+      isAsync: true,
+      validator: function(rating, callback) {
+        setTimeout(() => {
+          const result = 0 <= rating && rating <= 5;
+          callback(result);
+        }, 2000);
+      },
+      message: 'Path `rating` should be in between 0 and 5'
+    }
+  }
 });
 
 const Course = mongoose.model('courses', courseSchema);
 
-async function createCourse(name, author, tags, price) {
+async function createCourse(data) {
+  const course = new Course({ ...data });
   try {
-    const course = new Course({ name, author, tags, price });
-    return await course.save();
-  } catch (err) {
-    return err.message;
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    for (let err in ex.errors) {
+      console.log(ex.errors[err].message);
+    }
   }
 }
+
+const courseData = {
+  name: 'Angular Master',
+  author: 'Brooks Benson',
+  category: 'Frontend',
+  tags: 'programming',
+  price: 10,
+  rating: 5
+};
+
+createCourse(courseData);
